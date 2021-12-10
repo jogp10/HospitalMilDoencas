611ba3d9d9af113b55714f13edd25a2e4da41ada
zearaujo11 <72151399+zearaujo11@users.noreply.github.com>
2021-12-10 15:57:19 +0000

    Python script stated writing

--- Python/file.py
+++ Python/file.py
@@ -1,4 +1,27 @@
- 
-        
-        
-        
+# inset into PACIENTE values(); 
+import random
+
+first_names = {"José ", "Bernardo ", "Francisca ", "Francisco ", 
+         "Matilde " , "João ", "Ricardo ", "Anabela ", 
+         "Ana ", "Joana ", "Guilherme ", "Angelo ", 
+         "Júlia ", "Juliano ", "André ", "Pedro ", 
+         "Paula ", "Paulo ", "Jorge ", "Afonso ", 
+         "Elisabete ", "Anderson "}
+
+last_names = {"Araújo", "Cavalheiro", "Pinheiro", 
+              "Barbosa", "Luís", "Gigante", 
+              "Almeida", "Pereira", "Silva", 
+              "Moreira", "Abdul", "Bianca", 
+              "Guedes", "Vieira", "Veiga", 
+              "Peixe", "Basílico", "Mercedes"}
+
+
+names = set()      
+       
+
+while len(names) <= 40 :
+    name = random.choice(first_names) + random.choice(last_names)
+    names.add(name)
+    
+for i in names: 
+    print (i)
