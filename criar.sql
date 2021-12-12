246185c4762be604ec8b5e221a3682960837d273
jogp10 <jogp10@gmail.com>
2021-12-12 18:29:01 +0000

    Finishing criar.sql

--- criar.sql
+++ criar.sql
@@ -16,125 +16,124 @@ drop table if exists DATAdeNASCIMENTO;
 
 
 create table PACIENTE (
-	idPaciente INTEGER PRIMARY KEY,
-	Telefone INTEGER UNIQUE,
-	Nome TEXT NOT NULL,
-	Morada TEXT DEFAULT '',
-	DataDeNascimento DATE NOT NULL,
-	Genero TEXT NOT NULL,
-	Profissao  TEXT DEFAULT ''
+	idPaciente 		INTEGER PRIMARY KEY,
+	Telefone 		INTEGER UNIQUE,
+	Nome 			TEXT 	NOT NULL,
+	Morada			TEXT 	DEFAULT '',
+	DataDeNascimento	DATE 	NOT NULL,
+	Genero 		TEXT 	NOT NULL,
+	Profissao  		TEXT 	DEFAULT '',
 	
+	CONSTRAINT genero CHECK (Genero = ('M' or 'F' or 'O'))
 		
 );
 
 create table PESSOAaCONTACTAR (
-	idPessoaAContactar INTEGER PRIMARY KEY,
-	Telefone INTEGER UNIQUE,
-	Nome TEXT NOT NULL,
-	Morada TEXT DEFAULT '',
-	DataDeNascimento DATE NOT NULL,
-	Genero TEXT NOT NULL
+	idPessoaAContactar 	INTEGER PRIMARY KEY,
+	Telefone 		INTEGER UNIQUE,
+	Nome			TEXT 	NOT NULL,
+	Morada			TEXT 	DEFAULT '',
+	DataDeNascimento	DATE 	NOT NULL,
+	Genero 		TEXT NOT NULL,
+	
+	CONSTRAINT genero CHECK(Genero = ('M' or 'F' or 'O'))
 
 );
 
 create table COLABORADOR (
-	idColaborador INTEGER PRIMARY KEY,
-	Telefone INTEGER UNIQUE,
-	Nome TEXT NOT NULL,
-	Morada TEXT DEFAULT '',
-	DataDeNascimento DATE NOT NULL,
-	Genero TEXT NOT NULL,
-	Estatuto TEXT,
-	Comida TEXT,
-	Especialidade TEXT 
-			CONSTRAINT fk_colaborador_especialidade REFERENCES ESPECIALIDADE (Nome) ON DELETE CASCADE
-													ON UPDATE CASCADE,
-	CHECK((Estatuto = NOT NULL and Comida = NULL and Especialidade = NULL) or
+	idColaborador 		INTEGER PRIMARY KEY,
+	Telefone 		INTEGER UNIQUE,
+	Nome 			TEXT 	NOT NULL,
+	Morada 		TEXT 	DEFAULT '',
+	DataDeNascimento 	DATE 	NOT NULL,
+	Genero 		TEXT 	NOT NULL,
+	Estatuto 		TEXT,
+	Comida 		TEXT,
+	Especialidade 		TEXT 			CONSTRAINT fk_colaborador_especialidade REFERENCES ESPECIALIDADE (Nome) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+													
+	CONSTRAINT tipoColaborador_colaborador CHECK(
+		(Estatuto = NOT NULL and Comida = NULL and Especialidade = NULL) or
 		(Estatuto = NULL and Comida = NOT NULL and Especialidade = NULL) or
-		(Estatuto = NULL and Comida = NULL and Especialidade = NOT NULL)
+		(Estatuto = NULL and Comida = NULL and Especialidade = NOT NULL)),
+		
+	CONSTRAINT genero CHECK(Genero = ('M' or 'F' or 'O'))
 
 );
 
 create table GRAUdePARENTESCO (
-	idPaciente INTEGER PRIMARY KEY
-			CONSTRAINT fk_graudeparentesco_idpaciente REFERENCES PACIENTE (idPaciente) ON DELETE CASCADE
-													ON UPDATE CASCADE,
-	idPessoaAContactar INTEGER 
-			CONSTRAINT fk_graudeparentesco_idpessoaacontactar REFERENCES PESSOAaCONTACTAR (idPessoaAContactar) ON DELETE SET NULL
-													  			ON UPDATE CASCADE,
-		CHECK(idPaciente in (select idPaciente from PACIENTE)),
-		CHECK(idPessoaAContactar in (select idPessoaAContactar from PESSOAaCONTACTAR))
+	idPaciente 		INTEGER PRIMARY KEY	CONSTRAINT fk_graudeparentesco_idpaciente REFERENCES PACIENTE (idPaciente) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+	idPessoaAContactar 	INTEGER 		CONSTRAINT fk_graudeparentesco_idpessoaacontactar REFERENCES PESSOAaCONTACTAR (idPessoaAContactar) 
+												ON DELETE SET NULL ON UPDATE CASCADE
+													  	
 													  				
 );
 
 create table ESPECIALIDADE (
-	idEspecialidade INTEGER PRIMARY KEY,
-	Nome TEXT NOT NULL
+	idEspecialidade 	INTEGER PRIMARY KEY,
+	Nome 			TEXT 	NOT NULL
 
 );
 
 create table PATOLOGIA (
-	idPatologia INTEGER PRIMARY KEY,
-	Nome TEXT NOT NULL
+	idPatologia 		INTEGER PRIMARY KEY,
+	Nome 			TEXT 	NOT NULL
 
 );
 
 create table SEGUROSdeSAUDE (
-	idSegurosDeSaude INTEGER PRIMARY KEY,
-	Nome TEXT NOT NULL,
-	Validade DATE NOT NULL
+	idSegurosDeSaude 	INTEGER PRIMARY KEY,
+	Nome 			TEXT 	NOT NULL,
+	Validade 		DATE 	NOT NULL
 
 );
 
 create table PacienteSegurosPatologia (
-	idPaciente INTEGER
-			CONSTRAINT fk_pacientesegurospatologia_idpaciente REFERENCES PACIENTE (idPaciente) ON DELETE CASCADE
-											       		ON UPDATE CASCADE,
-	idPatologia INTEGER
-			CONSTRAINT fk_pacientesegurospatologia_idpatologia REFERENCES PATOLOGIA (idPatologia) ON DELETE CASCADE
-											       			ON UPDATE CASCADE,
-	idSegurosDeSaude INTEGER
-			CONSTRAINT fk_pacientesegurospatologia_idsegurosdesaude REFERENCES SEGUROSdeSAUDE (idSegurosDeSaude) ON DELETE CASCADE
-											       					ON UPDATE CASCADE,
+	idPaciente 		INTEGER		CONSTRAINT fk_pacientesegurospatologia_idpaciente REFERENCES PACIENTE (idPaciente) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+	idPatologia 		INTEGER		CONSTRAINT fk_pacientesegurospatologia_idpatologia REFERENCES PATOLOGIA (idPatologia) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+	idSegurosDeSaude 	INTEGER		CONSTRAINT fk_pacientesegurospatologia_idsegurosdesaude REFERENCES SEGUROSdeSAUDE (idSegurosDeSaude) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
 											    	
 	PRIMARY KEY (
         	idPaciente,
         	idPatologia,
         	idSegurosDeSaude
-        	),
-        	
-        CHECK(idPaciente in (select idPaciente from PACIENTE)),
-        CHECK(idPatologia in (select idPatologia from PATOLOGIA)),
-        CHECK(idSegurosDeSaude in (select idSegurosDeSaude from SEGUROSdeSAUDE))
+        	)
+
 
 );
 
 create table TRATA (
-	idPatologia INTEGER PRIMARY KEY
-			CONSTRAINT fk_trata_idpaciente REFERENCES PACIENTE (idPaciente) ON DELETE CASCADE
-											       ON UPDATE CASCADE,
-	idMedico INTEGER NOT NULL
-			CONSTRAINT fk_trata_idmedico REFERENCES COLABORADOR (idColaborador) ON DELETE CASCADE
-												ON UPDATE CASCADE,
-	Tratamento TEXT DEFAULT ''
+	idPatologia 		INTEGER PRIMARY KEY	CONSTRAINT fk_trata_idpaciente REFERENCES PACIENTE (idPaciente) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+	idMedico 		INTEGER NOT NULL	CONSTRAINT fk_trata_idmedico REFERENCES COLABORADOR (idColaborador) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+	Tratamento 		TEXT DEFAULT ''
 );
 
 create table AGENDA (
-	idAgenda INTEGER PRIMARY KEY,
-	HoraInicial TIME NOT NULL,
-	HoraFinal TIME NOT NULL,
-	Dia DATE NOT NULL,
-	Local TEXT NOT NULL,
-	idPaciente INTEGER 
-			CONSTRAINT fk_agenda_idpaciente REFERENCES PACIENTE (idPaciente) ON DELETE CASCADE
-												ON UPDATE CASCADE,
-	idColaborador INTEGER 
-			CONSTRAINT fk_agenda_idcolaborador REFERENCES COLABORADOR (idColaborador) ON DELETE CASCADE
-													ON UPDATE CASCADE
+	idAgenda 		INTEGER PRIMARY KEY,
+	HoraInicial 		TIME 	NOT NULL,
+	HoraFinal 		TIME 	NOT NULL,
+	Dia 			DATE 	NOT NULL,
+	Local 			TEXT 	NOT NULL,
+	idPaciente 		INTEGER 		CONSTRAINT fk_agenda_idpaciente REFERENCES PACIENTE (idPaciente) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+	idColaborador 		INTEGER 		CONSTRAINT fk_agenda_idcolaborador REFERENCES COLABORADOR (idColaborador) 
+												ON DELETE CASCADE ON UPDATE CASCADE,
+												
+        CONSTRAINT agendadequem_agenda CHECK(
+        	(idPaciente = NULL and idColaborador = NOT NULL) or
+        	(idPaciente = NOT NULL and idColaborador = NULL))								
 
 );
 
 create table DATAdeNASCIMENTO (
-	DataDeNascimento DATE PRIMARY KEY,
-	Idade INTEGER NOT NULL
+	DataDeNascimento 	DATE 	PRIMARY KEY,
+	Idade 			INTEGER NOT NULL
+	
 );
+
