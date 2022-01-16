a78b56cc3c00ef545a37736c27a618af2cad46e5
José Araújo <ze.b.araujo@outlook.com>
2022-01-16 16:54:17 +0000

    int4

--- 3rd delivery/queries/int4.sql
+++ 3rd delivery/queries/int4.sql
@@ -1,3 +1,10 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+# Nomes das especialidades e nr de medicos que trabalham em cada uma
+
+select Especialidade.Nome as Especialidade, count(*) as nr_medicos
+from Especialidade, Colaborador
+where Especialidade.idEspecialidade = Colaborador.idEspecialidade
+group by Especialidade.Nome ; 
