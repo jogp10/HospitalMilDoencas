8265e44fe541cbe10dbf2487f006e2268b2388b3
RicardoCavalheiro123 <ricardo18-4-2001@hotmail.com>
2022-01-16 18:36:41 +0000

    int7 e int8

--- 3rdDelivery/queries/int7.sql
+++ 3rdDelivery/queries/int7.sql
@@ -1,3 +1,8 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+SELECT Paciente.Nome, Paciente.DataDeNascimento
+FROM Paciente
+WHERE Paciente.DataDeNascimento BETWEEN '1990' AND '2000'
+ORDER BY DataDeNascimento DESC;
--- 3rdDelivery/queries/int8.sql
+++ 3rdDelivery/queries/int8.sql
@@ -1,3 +1,9 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+
+Select Colaborador.nome, Especialidade.Nome, Agenda.HoraInicial, Agenda.HoraFinal, max(Agenda.HoraFinal- Agenda.HoraInicial) as CargaHoraria
+From Colaborador, Especialidade, Agenda
+Where Colaborador.idEspecialidade = Especialidade.idEspecialidade and Agenda.idColaborador = Colaborador.idColaborador
+;
