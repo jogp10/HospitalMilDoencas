179a225b54dcc34a5cbfa46f55c1f8758e357f58
José Araújo <ze.b.araujo@outlook.com>
2022-01-16 19:22:48 +0000

    int6.sql

--- 3rdDelivery/queries/int4.sql
+++ 3rdDelivery/queries/int4.sql
@@ -4,7 +4,7 @@
 
 # Nomes das especialidades e nr de medicos que trabalham em cada uma
 
-select Especialidade.Nome as Especialidade, count(*) as nr_medicos
+select count(*) as nr_medicos, Especialidade.Nome as Especialidade
 from Especialidade, Colaborador
 where Especialidade.idEspecialidade = Colaborador.idEspecialidade
 group by Especialidade.Nome 
--- 3rdDelivery/queries/int6.sql
+++ 3rdDelivery/queries/int6.sql
@@ -1,3 +1,11 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+# Patologias e tratamento para essa patologia, nr de medicos capazes de tratar
+
+select Patologia.Nome, Trata.tratamento as tratamento, count(*) as medicos
+from Patologia, Colaborador, Trata
+where Patologia.idPatologia = Trata.idPatologia and Colaborador.idColaborador = Trata.idMedico
+group by Trata.idPatologia, Trata.tratamento
+order by Patologia.Nome ASC; 
