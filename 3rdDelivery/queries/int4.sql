3061c7bf8f2ed9531e31e3f282c07451269ef56a
José Araújo <ze.b.araujo@outlook.com>
2022-01-16 17:02:41 +0000

    int4 changes

--- 3rdDelivery/queries/int4.sql
+++ 3rdDelivery/queries/int4.sql
@@ -7,4 +7,5 @@
 select Especialidade.Nome as Especialidade, count(*) as nr_medicos
 from Especialidade, Colaborador
 where Especialidade.idEspecialidade = Colaborador.idEspecialidade
-group by Especialidade.Nome ; 
+group by Especialidade.Nome 
+order by nr_medicos DESC; 
