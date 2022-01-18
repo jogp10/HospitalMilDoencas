a7b0059a69e8fe48091e2ea6408aa154dd5914ee
jogp10 <jogp10@gmail.com>
2022-01-18 15:38:41 +0000

    querie 8

--- 3rdDelivery/queries/int8.sql
+++ 3rdDelivery/queries/int8.sql
@@ -5,5 +5,5 @@
 
 Select Colaborador.nome, Especialidade.Nome, Agenda.HoraInicial, Agenda.HoraFinal, max(Agenda.HoraFinal- Agenda.HoraInicial) as CargaHoraria
 From Colaborador, Especialidade, Agenda
-Where Colaborador.idEspecialidade = Especialidade.idEspecialidade and Agenda.idColaborador = Colaborador.idColaborador
-;
+Where Colaborador.idEspecialidade = Especialidade.idEspecialidade and Agenda.idColaborador = Colaborador.idColaborador;
+
