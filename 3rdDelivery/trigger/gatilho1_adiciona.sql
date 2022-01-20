3451e88d59574462767ffceb357fb1b94e430665
RicardoCavalheiro123 <74145781+RicardoCavalheiro123@users.noreply.github.com>
2022-01-20 16:13:41 +0000

    Update gatilho1_adiciona.sql

--- 3rdDelivery/trigger/gatilho1_adiciona.sql
+++ 3rdDelivery/trigger/gatilho1_adiciona.sql
@@ -2,3 +2,15 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+-- Gatilho que impede de criar um Seguro de Saúde com 
+-- uma Validade anterior à data atual
+
+CREATE TRIGGER SeguroInvalido
+BEFORE INSERT ON SegurodeSaude
+FOR EACH ROW
+BEGIN
+	Select CASE
+		WHEN New.Validade < CAST(CURRENT_TIMESTAMP AS DATE)
+	THEN RAISE(ABORT, 'Validade inválida')
+END;
