c0c26e7f9adf422054c2e878c603199a626bc4ea
José Araújo <ze.b.araujo@outlook.com>
2022-01-20 18:26:09 +0000

    Adding a trigger and a txt to backup povoar.sql

--- 3rdDelivery/trigger/gatilho2_adiciona.sql
+++ 3rdDelivery/trigger/gatilho2_adiciona.sql
@@ -2,3 +2,15 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+create trigger apagapaciente
+after delete on PACIENTE
+referencing old row as pac 
+for each row 
+begin
+    delete from GRAUdePARENTESCO where pac.idPaciente = GRAUdePARENTESCO.idPaciente 
+
+    delete from PESSOAaCONTACTAR where PESSOAaCONTACTAR.idPessoaAContactar = GRAUdePARENTESCO.old.idPessoaAContactar and PESSOAaCONTACTAR.idPessoaAContactar not in 
+                                       (select *
+                                        from GRAUdePARENTESCO )                           
+end; 
--- 3rdDelivery/trigger/gatilho2_verifica.sql
+++ 3rdDelivery/trigger/gatilho2_verifica.sql
@@ -2,3 +2,20 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+.print ''
+
+select *
+from PACIENTE
+
+.print ''
+
+select * 
+from PESSOAaCONTACTAR
+
+.print ''
+.print 'Ao apagar um paciente, apagamos o respetivo contacto de emergencia se este nao for de mais nenhum paciente; '
+.print ''
+
+delete from PACIENTE where PACIENTE.idPaciente = 62; 
+
