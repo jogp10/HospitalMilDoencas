25f0a8a7e978eedd51bd47e6a825bd390abb53aa
jogp10 <jogp10@gmail.com>
2022-01-21 14:16:27 +0000

    deleted sql files

--- 3rdDelivery/trigger/gatilho1_adiciona.sql
+++ 3rdDelivery/trigger/gatilho1_adiciona.sql
@@ -4,8 +4,6 @@
 PRAGMA foreign_keys = ON;
 
 -- Gatilho que impede de alterar um Seguro de Saúde para uma Validade inválida
-
-
 CREATE TRIGGER IF NOT EXISTS SeguroInvalido
 BEFORE INSERT ON SEGUROdeSAUDE
 WHEN (strftime('%Y', 'now') - strftime('%Y', NEW.Validade) < 0)
