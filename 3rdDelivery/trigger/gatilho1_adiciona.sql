344408a18a683aab6741e24928a1e9229ec2bd71
RicardoCavalheiro123 <ricardo18-4-2001@hotmail.com>
2022-01-23 23:43:15 +0000

    1

--- 3rdDelivery/trigger/gatilho1_adiciona.sql
+++ 3rdDelivery/trigger/gatilho1_adiciona.sql
@@ -3,10 +3,18 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
--- Gatilho que impede de alterar um Seguro de Saúde para uma Validade inválida
+-- Gatilho que impede de adicionar um Seguro de Saúde para uma Validade inválida
 CREATE TRIGGER IF NOT EXISTS SeguroInvalido
 BEFORE INSERT ON SEGUROdeSAUDE
 WHEN strftime('%Y-%m-%d', 'now') > NEW.Validade
 BEGIN
     SELECT RAISE(ROLLBACK, 'Seguro invalido!');
 END;
+
+CREATE TRIGGER IF NOT EXISTS SeguroInvalidoUpdate
+BEFORE UPDATE OF Validade ON SEGUROdeSAUDE
+FOR EACH ROW
+WHEN strftime('%Y-%m-%d', 'now') > NEW.Validade
+  BEGIN
+    SELECT RAISE(ROLLBACK, 'Seguro inválido!');
+  END;
--- 3rdDelivery/trigger/gatilho1_verifica.sql
+++ 3rdDelivery/trigger/gatilho1_verifica.sql
@@ -12,7 +12,7 @@ insert into SEGUROdeSAUDE values(701,'Liberty', '2034-01-01');
 
 SELECT Nome,Validade FROM SEGUROdeSAUDE;
 .print ''
-.print 'Tentar alterar a validade do Seguro de Saude da Multicare para disparar o gatilho;'
+.print 'Tentar adicionar um Seguro de Saude com uma Validade inválida;'
 .print ''
 .print 'Validade = 2010 (Inválido) :'
 .print ''
@@ -24,10 +24,26 @@ insert into SEGUROdeSAUDE values(702,'AXA','2010-01-01');
 Select Nome,Validade FROM SEGUROdeSAUDE;
 
 .print ''
-.print 'Alterando para uma válida'
+.print 'Adicionando uma válida'
 .print 'Validade = 2040 (Válido) :'
 .print ''
 
 
-insert into SEGUROdeSAUDE values(703,'D','2040-01-01');
+insert into SEGUROdeSAUDE values(703,'Servicos Sociais','2040-01-01');
+Select Nome,Validade FROM SEGUROdeSAUDE;
+
+.print ''
+.print 'Alterar a validade do Seguro da Multicare para uma Validade inválida'
+.print 'Validade = 2005 (Inválido) :'
+.print ''
+
+UPDATE SEGUROdeSaude SET Validade = '2005-02-20' WHERE SEGUROdeSAUDE.Nome = 'Multicare';
+
+.print ''
+.print 'Alterar a validade do Seguro da Liberty para uma Validade válida'
+.print 'Validade = 2050 (Válido) :'
+.print ''
+
+UPDATE SEGUROdeSaude SET Validade = '2050-02-20' WHERE SEGUROdeSAUDE.Nome = 'Liberty';
+
 Select Nome,Validade FROM SEGUROdeSAUDE;
