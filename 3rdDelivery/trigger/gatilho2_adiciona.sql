999f613567e88fa15fc1c180a9b379c2cf52b604
José Araújo <ze.b.araujo@outlook.com>
2022-01-23 22:16:23 +0000

    Ending last trigger

--- 3rdDelivery/trigger/gatilho2_adiciona.sql
+++ 3rdDelivery/trigger/gatilho2_adiciona.sql
@@ -3,15 +3,10 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-create trigger apagapaciente
+create trigger if not exists apagaPaciente
 after delete on PACIENTE
-referencing old row as pac 
 for each row 
 begin
-	referencing (select GRAUdePARENTESCO.idPessoaAContactar from GRAUdePARENTESCO where pac.idPaciente = GRAUdePARENTESCO.idPaciente) as idPessoaADeletar
-    delete from GRAUdePARENTESCO where pac.idPaciente = GRAUdePARENTESCO.idPaciente 
-
-    delete from PESSOAaCONTACTAR where PESSOAaCONTACTAR.idPessoaAContactar  not in idPessoaADeletar and PESSOAaCONTACTAR.idPessoaAContactar not in 
-                                       (select *
-                                        from GRAUdePARENTESCO )                           
+    delete from GRAUdePARENTESCO where old.idPaciente = GRAUdePARENTESCO.idPaciente ; 
+    delete from PESSOAaCONTACTAR where PESSOAaCONTACTAR.idPessoaAContactar not in (select idPessoaAContactar from GRAUdePARENTESCO); 
 end; 
