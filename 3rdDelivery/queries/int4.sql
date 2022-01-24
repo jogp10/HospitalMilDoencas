f728e18ecce7ff3f7f7eaba420ea7deb577039c1
RicardoCavalheiro123 <ricardo18-4-2001@hotmail.com>
2022-01-24 00:02:27 +0000

    4 e 9

--- 3rdDelivery/queries/int4.sql
+++ 3rdDelivery/queries/int4.sql
@@ -1,11 +1,11 @@
-.mode columns
-.headers on
-.nullvalue NULL
-
-# Nomes das especialidades e nr de medicos que trabalham em cada uma
-
-select Especialidade.Nome as Especialidade, count(*) as nr_medicos
-from Especialidade, Colaborador
-where Especialidade.idEspecialidade = Colaborador.idEspecialidade
-group by Especialidade.Nome 
-order by nr_medicos DESC; 
+.mode columns 
+.headers on 
+.nullvalue NULL 
+ 
+# Media de idades por patologia 
+ 
+select patologia.nome, round(avg(datadenascimento.idade), 0) as media_idades 
+from Patologia, Paciente, datadenascimento, PacienteSeguroPatologia
+where paciente.idPaciente=PacienteSeguroPatologia.idPaciente and PacienteSeguroPatologia.idPatologia=Patologia.idPatologia and paciente.datadenascimento=datadenascimento.datadenascimento
+group by patologia.nome
+order by media_idades desc;
--- 3rdDelivery/queries/int9.sql
+++ 3rdDelivery/queries/int9.sql
@@ -2,14 +2,13 @@
 .headers on
 .nullvalue NULL
 
-# Quantos medicos podem tratar o paciente 
 
-create view nomePaciente as 
-Select Paciente.Nome as Nome, Paciente.idPaciente as id_Paciente, TRATA.idMedico, PacienteSeguroPatologia.idPatologia, Colaborador.Nome
-From Paciente, TRATA, PacienteSeguroPatologia, Colaborador
-Where Paciente.idPaciente = PacienteSeguroPatologia.idPaciente and PacienteSeguroPatologia.idPatologia  = Trata.idPatologia and Colaborador.idColaborador = Trata.idMedico
+create view if not exists nomePaciente as 
+Select Paciente.Nome as Nome, Paciente.idPaciente as id, Patologia.Nome as Patologia, TRATA.idMedico, PacienteSeguroPatologia.idPatologia, Colaborador.Nome
+From Paciente, TRATA, PacienteSeguroPatologia, Colaborador,Patologia
+Where Paciente.idPaciente = PacienteSeguroPatologia.idPaciente and PacienteSeguroPatologia.idPatologia  = Trata.idPatologia and Colaborador.idColaborador = Trata.idMedico and Patologia.idPatologia = PacienteSeguroPatologia.idPatologia
 order by Paciente.Nome; 
 
-Select Nome, id_Paciente, count(*) as nr_medicos from nomePaciente
-group by id_Paciente
-order by nome asc;
+Select NomePaciente.Nome, Patologia, count(*) as Nr_medicos from nomePaciente
+group by id
+order by NomePaciente.Nome asc;
